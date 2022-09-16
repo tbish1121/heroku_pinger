2dff0c5c72a9ec1ed87c22ebf17ccfdc4c2ab839
Taylor <tbish1121@gmail.com>
2022-09-16 18:50:36 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import time
+import urllib.request
+
+run = True
+minutes = 24
+
+while run:
+    time.sleep(60)
+    minutes += 1
+    if minutes == 1:
+        print(f'{minutes} minute has elapsed.')
+    elif (minutes > 1) and (minutes < 4):
+        print(f'{minutes} minutes have elapsed')
+    elif minutes == 25:
+        page = urllib.request.urlopen('http://www.taylorbish.com')
+        page.read()
+        minutes = 0
+        print("Page loaded")
